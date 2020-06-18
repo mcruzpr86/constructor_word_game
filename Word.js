6902903b81ed296b31e99a1ae8e20deea0364115
mcruz <mcruzpr86@gmail.com>
2020-06-17 22:53:04 -0400

    word function

--- Word.js
+++ Word.js
@@ -1,5 +1,10 @@
-var Letter = require("Letter")
+var Letter = require("./Letter.js")
 
 function Word(currentWord) {
-
+    this.currentWord = currentWord
+    this.newLetter = currentWord.split('').map(function (val) {
+        return new Letter(val)
+    })
 }
+let test = new Word('Marvel')
+console.log(test.newLetter)
