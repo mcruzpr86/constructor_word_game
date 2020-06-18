99a14b50c62bec4920612cff0e8c8c9593c182a3
mcruz <mcruzpr86@gmail.com>
2020-06-17 23:17:56 -0400

    word function

--- Word.js
+++ Word.js
@@ -5,6 +5,15 @@ function Word(currentWord) {
     this.newLetter = currentWord.split('').map(function (val) {
         return new Letter(val)
     })
+    this.displayWord = function () {
+        this.currentWord = []
+        for (let i = 0; i < newLetter.length; i++) {
+            this.wordArr.push(this.newLetter[i].guessed())
+
+        }
+    }
 }
+
 let test = new Word('Marvel')
 console.log(test.newLetter)
+
