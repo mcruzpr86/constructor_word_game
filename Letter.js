ca6e12c0565c709eb83811dfb30d25e11db9c3eb
mcruz <mcruzpr86@gmail.com>
2020-06-17 22:39:19 -0400

    letterjs file establisehd with export to index file

--- Letter.js
+++ Letter.js
@@ -1,22 +1,23 @@
 function Letter(char) {
     this.char = char;
-    this.characterArray = this.char.split("")
-    console.log(this.characterArray)
+    this.guessedWord = false
 
     //method
     this.showBlanks = function () {
-        this.blanks = []
-        for (var i = 0; i < this.characterArray.length; i++) {
-            this.blanks.push("_ ")
-
+        if (this.guessedWord) {
+            return this.char
+        } else {
+            return "_ "
         }
-        console.log(this.blanks.join(""))
+
+    }
+    this.checkGuess = function (userGuess) {
+        if (userGuess === this.char) {
+            this.guessedWord = true
+        }
     }
 }
 
 //create method that checks letter if correct
 
-
-var test = new Letter("test")
-console.log(test.word)
 module.exports = Letter;
